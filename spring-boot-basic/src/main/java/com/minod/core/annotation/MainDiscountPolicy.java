34b9599a65ed43c155a6067f3c40f31d7036267b
dominod <dbdomino@naver.com>
2024-02-11 21:49:13 +0900

    어노테이션 만들기

--- spring-boot-basic/src/main/java/com/minod/core/annotation/MainDiscountPolicy.java
+++ spring-boot-basic/src/main/java/com/minod/core/annotation/MainDiscountPolicy.java
@@ -2,6 +2,8 @@ package com.minod.core.annotation;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.lang.annotation.*;
+
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
