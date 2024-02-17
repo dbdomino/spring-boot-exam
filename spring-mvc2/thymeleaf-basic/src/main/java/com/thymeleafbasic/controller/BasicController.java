590a1c3031ecd12ec656232380501707c4c0b936
dominod <dbdomino@naver.com>
2024-02-17 22:20:37 +0900

    thymeleaf 컨트롤러

---
+++ spring-mvc2/thymeleaf-basic/src/main/java/com/thymeleafbasic/controller/BasicController.java
@@ -0,0 +1,61 @@
+package com.thymeleafbasic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "hello 안녕~~");
+        model.addAttribute("data2", "hello <b>안녕~</b>"); // html에 가면 <b>tag는 적용안된다. 문자그대로 나옴.
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("userList", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    static class User {
+        private String username;
+        private int age;
+
+        User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
