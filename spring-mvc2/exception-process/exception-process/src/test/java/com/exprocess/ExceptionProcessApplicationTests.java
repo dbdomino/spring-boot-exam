6fcc24991636c84eff1d948f4dd4ed55df0784a2
dominod <dbdomino@naver.com>
2024-02-22 08:27:46 +0900

    실행

---
+++ spring-mvc2/exception-process/exception-process/src/test/java/com/exprocess/ExceptionProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
