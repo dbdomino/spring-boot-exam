cf90c9dc23eb942fb2bb1abd4c86511673adf7b3
dominod <dbdomino@naver.com>
2024-02-23 22:59:40 +0900

    서비스 자바로직

---
+++ spring-db-1/db-jdbc/src/main/java/com/dbjdbc/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,28 @@
+package com.dbjdbc.jdbc.service;
+
+import com.dbjdbc.domain.Member;
+import com.dbjdbc.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+    private final MemberRepositoryV1 memberRepositoryV1;
+
+    // 계좌이체 예시 만들기
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV1.findById(fromId);
+        Member toMember = memberRepositoryV1.findById(toId);
+
+        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외발생, 맴버ID ex는 입금금지");
+        }
+    }
+}
