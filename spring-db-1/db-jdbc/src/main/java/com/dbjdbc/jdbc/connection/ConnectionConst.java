b42c09e55880e9a618f0ef580f9104b24113a6c8
dominod <dbdomino@naver.com>
2024-02-23 22:56:32 +0900

    DB 연결

---
+++ spring-db-1/db-jdbc/src/main/java/com/dbjdbc/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package com.dbjdbc.jdbc.connection;
+
+public class ConnectionConst {
+    public static final String URL = "jdbc:postgresql://localhost:5432/book";
+    public static final String USERNAME = "minod";
+    public static final String PASSWORD = "minodminod";
+
+}
