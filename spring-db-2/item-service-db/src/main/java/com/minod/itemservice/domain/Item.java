574bbf80ef2b3e4a9c80396b9e15650fd2a49637
dominod <dbdomino@naver.com>
2024-02-27 22:56:31 +0900

    domain

---
+++ spring-db-2/item-service-db/src/main/java/com/minod/itemservice/domain/Item.java
@@ -0,0 +1,38 @@
+package com.minod.itemservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+//@Data  // Argument, Constructor, ToString, Equals, Hashcode 다만들어준다. 위험할수 있으므로 자제하는게좋다.
+@Getter
+@Setter // 게터 세터만 써도 충분하긴하다.
+@ToString
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    // 추가 변수, 옵션임.
+    private Boolean open; // 판매여부
+    private List<String> regions; // 등록지역
+    private ItemType itemType; // 상품종류
+    private String deliveryCode; // 배송코드, 배송방식
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public  Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
