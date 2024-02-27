3f4cadca1aadb685fdb770367f3e9a7d31486c98
dominod <dbdomino@naver.com>
2024-02-27 22:56:45 +0900

    repository

---
+++ spring-db-2/item-service-db/src/main/java/com/minod/itemservice/repository/ItemRepository.java
@@ -0,0 +1,18 @@
+package com.minod.itemservice.repository;
+
+import com.minod.itemservice.domain.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository {
+
+    Item save(Item item);
+
+    void update(Long itemId, ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond cond);
+
+}
