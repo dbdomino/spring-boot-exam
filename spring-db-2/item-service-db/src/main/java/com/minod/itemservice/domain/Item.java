90d280d6a466a01fd79084a3453218e12670337f
dominod <dbdomino@naver.com>
2024-02-28 23:41:18 +0900

    JPA Entity 변경

--- spring-db-2/item-service-db/src/main/java/com/minod/itemservice/domain/Item.java
+++ spring-db-2/item-service-db/src/main/java/com/minod/itemservice/domain/Item.java
@@ -1,26 +1,27 @@
 package com.minod.itemservice.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.Data;
 
 //@Data  // Argument, Constructor, ToString, Equals, Hashcode 다만들어준다. 위험할수 있으므로 자제하는게좋다.
-@Getter
-@Setter // 게터 세터만 써도 충분하긴하다.
-@ToString
+@Data
+@Entity // 이 에노테이션이 있어야 JPA가 인식할 수 있다. @Entity 가 붙은 객체를 JPA에서는 엔티티라 한다.
 public class Item {
+
+    @Id  // 기본키일 경우 알려줘야 한다고 함. 테이블의 PK와 해당 필드를 매핑한다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에서 ID를 생성해주는 전략이므로 GenerationType.IDENTITY를 써줘야 된다고 함.
     private Long id;
+    @Column(name = "item_name", length = 10) // length 는 안써도 됨.
     private String itemName;
+
     private Integer price;
     private Integer quantity;
 
-    // 추가 변수, 옵션임.
-    private Boolean open; // 판매여부
-    private List<String> regions; // 등록지역
-    private ItemType itemType; // 상품종류
-    private String deliveryCode; // 배송코드, 배송방식
+    // jpa에선 엔티티와 일치하지 않는 필드 있으면 오류 발생 [오류: "delivery_code" 칼럼은 "item" 릴레이션(relation)에 없음,
+    // [insert into public.item (delivery_code,item_name,item_type,open,price,quantity) values (?,?,?,?,?,?)]; SQL [insert into public.item (delivery_code,item_name,item_type,open,price,quantity) values (?,?,?,?,?,?)]
+//    private Boolean open; // 판매여부
+//    private ItemType itemType; // 상품종류
+//    private String deliveryCode; // 배송코드, 배송방식
 
     public Item(String itemName) {
         this.itemName = itemName;
