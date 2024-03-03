655b44f0003b20fae121375ec54fed7b688cdfbc
dominod <dbdomino@naver.com>
2024-03-03 23:38:25 +0900

    트랜잭션 테스트

--- spring-db-2/transaction/src/test/java/com/minod/TransactionApplicationTests.java
+++ spring-db-2/transaction/src/test/java/com/minod/TransactionApplicationTests.java
@@ -1,13 +1,22 @@
 package com.minod;
 
+import com.minod.aws.CustomApplicationContextInitializer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class TransactionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// 테스트에서 addInitializer 수행되도록 하기
+		SpringApplication application = new SpringApplication(TransactionApplication.class);
+		application.addInitializers(new CustomApplicationContextInitializer());  // 초기화작업으로 시크릿매니저 값 프로퍼티에 추가
+		ConfigurableApplicationContext context = application.run();
+		// 테스트 내용 추가
+		context.close(); // 테스트 종료 후 애플리케이션 컨텍스트 닫기
 	}
 
 }
