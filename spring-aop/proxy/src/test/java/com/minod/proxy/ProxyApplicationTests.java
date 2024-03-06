a508c74723045e7ff927315c9117bcbb47b6b7e3
dominod <dbdomino@naver.com>
2024-03-06 11:34:55 +0900

    테스트 실행

---
+++ spring-aop/proxy/src/test/java/com/minod/proxy/ProxyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.minod.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+@SpringBootTest(classes = ProxyApplicationTests.class)
+public class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
