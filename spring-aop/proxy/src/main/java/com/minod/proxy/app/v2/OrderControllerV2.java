d397095676e8c68b791b180166dcb84ef524b49d
dominod <dbdomino@naver.com>
2024-03-06 11:37:14 +0900

    구체 클래스 기반 로그추적기 서비스

---
+++ spring-aop/proxy/src/main/java/com/minod/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,26 @@
+package com.minod.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @RequestMapping("/v2/request")
+    public String request(@RequestParam("itemId") String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+    @RequestMapping("/v2/nolog")
+    public String noLog() {
+        return "ok";
+    }
+
+}
