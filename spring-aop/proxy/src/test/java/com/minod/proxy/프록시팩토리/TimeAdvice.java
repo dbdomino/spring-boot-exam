e36ae3b9da7f925039a72fd84d162bb09aed5a19
dominod <dbdomino@naver.com>
2024-03-09 22:52:16 +0900

    프록시 팩토리, Advice 만들어 공통로직, 구체클래스 및 인터페이스 기반 클래스의 메서드를 동적으로 받아와 동적 프록시 구성하기

---
+++ spring-aop/proxy/src/test/java/com/minod/proxy/프록시팩토리/TimeAdvice.java
@@ -0,0 +1,23 @@
+package com.minod.proxy.프록시팩토리;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args); // invoke로 쓰는 기존 동적 프록시에서 메서드 실행이 아래처럼 바뀜
+// 스프링 프록시에서 target을 알아서 찾아 잡아줌
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
