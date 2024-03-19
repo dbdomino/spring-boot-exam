cdad9d1307fd641f53d66bc646dd2c39615c7c0f
dominod <dbdomino@naver.com>
2024-03-19 21:37:50 +0900

    AspectJ 표현식 @args

---
+++ spring-aop/aop/src/test/java/com/minod/aop/pointcut/AtArgs.java
@@ -0,0 +1,11 @@
+package com.minod.aop.pointcut;
+
+public class AtArgs {
+    /**@args
+     @args : 전달된 실제 인수의 런타임 타입이 주어진 타입의 애노테이션을 갖는 조인 포인트
+
+     설명
+     전달된 인수의 런타임 타입에 @Check 애노테이션이 있는 경우에 매칭한다.
+     @args(test.Check)
+     */
+}
