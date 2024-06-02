f05a39abd8a0072aa515ebc55b1c0e48e2769ac7
dominod <dbdomino@naver.com>
2024-06-02 18:35:51 +0900

    스프링핵심응용-was

--- spring-boot-was/01-server-start/server-start/src/main/java/com/minod/serverstart/servlet/TestServlet.java
+++ spring-boot-was/01-server-start/server-start/src/main/java/com/minod/serverstart/servlet/TestServlet.java
@@ -12,6 +12,7 @@ import java.io.IOException;
  * http://localhost:8080/test
  * 배포하기 위해 war로 말아서 톰캣에 올려야 한다.
  */
+// 어노테이션 방식으로 서블릿 만들기, 간편하다, 경로 하나당 정적매핑
 @WebServlet("/test")
 public class TestServlet extends HttpServlet {
     // servlet 양식에 맞춰 줘야한다... 그래야 서블릿 환경에서 webapplication 동작이 수행된다.
