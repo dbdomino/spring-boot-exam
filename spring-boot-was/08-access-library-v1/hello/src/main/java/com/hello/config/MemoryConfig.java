a1762f08105fd0a3382bdda018a475514e95c674
dominod <dbdomino@naver.com>
2024-06-19 01:17:02 +0900

    라이브러리 만들기, 일반 실행

---
+++ spring-boot-was/08-access-library-v1/hello/src/main/java/com/hello/config/MemoryConfig.java
@@ -0,0 +1,22 @@
+package com.hello.config;
+
+import com.hello.memory.MemoryController; // 외부 라이브러리로 불러온 것
+import com.hello.memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 외부 라이브러리의 클래스를 bean으로 등록한 것.
+// 자동구성을 활용하여 여기 Configuration을 별도 추가하지 않고도 bean으로 등록되도록 만들어보자.
+// 라이브러리 만드려면, 자동구성을 모르면 안되네...
+// 비결은 라이브러리 프로젝트에 Config 파일을 자동구성으로 추가해 주는 것이다.
+@Configuration
+public class MemoryConfig {
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+}
